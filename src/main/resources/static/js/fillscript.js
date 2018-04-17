7c73ed701b6eb943f16c414a288fa1daa208d2c5
carlos <carlosalvarado310.ca@gmail.com>
2018-04-17 01:07:10 -0600

    Corrección de errores

--- src/main/resources/static/js/fillscript.js
+++ src/main/resources/static/js/fillscript.js
@@ -6,35 +6,6 @@ function fillMap(map) {
     getLocations();
     getRoute();
 }
-function getRoute() {
-    $.ajax({
-        url: 'http://localhost:3377/testRou',
-        data: {
-            format: 'json'
-        },
-        error: function (e) {
-            console.error(e);
-
-        },
-        success: function (data) {
-            fillRoute(data);
-        },
-        type: 'GET'
-    });
-}
-function fillRoute(data) {
-    $.each(data, function (index, value) {
-        var flight = new google.maps.Polyline({
-            path: [ {lat: value.locationOne.latitude, lng:value.locationOne.longitude},{lat: value.locationTwo.latitude, lng:value.locationTwo.longitude}],
-            geodesic: true,
-            strokeColor: '#FF0000',
-            strokeOpacity: 1.0,
-            strokeWeight: 2
-        });
-
-        flight.setMap(westerosMap)
-    });
-}
 function getLocations() {
     $.ajax({
         url: 'http://localhost:3377/testLoc',
