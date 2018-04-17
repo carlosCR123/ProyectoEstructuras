7111148b662ea6859018a77db061ae4a4f748866
Unknown <carlosalvarado310.ca@gmail.com>
2018-04-16 19:49:24 -0600

    Auto stash before merge of "Carlos" and "master"

--- src/main/resources/static/js/fillscript.js
+++ src/main/resources/static/js/fillscript.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var westerosMap;
 function fillMap(map) {
     westerosMap = map;
@@ -31,9 +32,216 @@ function fill(data) {
             icon: '../imgs/Winterfell.png'
         });
         marker.addListener('click', function () {
-            console.log("test");
-            
             $('.locationName').text(value.name);
         });
+=======
+/*
+function fillMap(map) {
+    var marker;
+
+    marker = new google.maps.Marker({
+        position: { lat: 60.90262003187983, lng:10.283203125 },
+        map: map,
+        title: 'Castle black',
+        draggable: false
+    });
+    marker = new google.maps.Marker({
+        position: { lat: 43.751298804563255, lng: -2.9443359375 },
+        map: map,
+        title: 'Winterfell',
+        draggable: false
+    });
+    
+    marker = new google.maps.Marker({
+        position: { lat: 29.178946061598406, lng: 5.888671875 },
+        map: map,
+        title: 'White Harbor',
+        draggable: false
+    });
+    marker = new google.maps.Marker({
+        position: { lat: 10.426153994763697, lng: -6.21573235356891 },
+        map: map,
+        title: 'Gray Water',
+        draggable: false
+    });
+    marker = new google.maps.Marker({
+        position: { lat: -1.5342492974085424, lng: -24.91121198499286 },
+        map: map,
+        title: 'Ironman bay',
+        draggable: false
+    });
+    marker = new google.maps.Marker({
+        position: { lat: -4.325063069159666, lng: -30.027938741088747 },
+        map: map,
+        title: 'Pyke',
+        draggable: false
+    });
+    marker = new google.maps.Marker({
+        position: { lat:  2.304275976243123, lng:  -8.886461513877748 },
+        map: map,
+        title: 'Seagard',
+        draggable: false
+    });
+    marker = new google.maps.Marker({
+        position: { lat:  2.525474119560536, lng: 18.311905008911253 },
+        map: map,
+        title: 'The Eyrie',
+        draggable: false
+    });
+    marker = new google.maps.Marker({
+        position: { lat: -4.675541120670668, lng:   34.13221750891125 },
+        map: map,
+        title: 'RuneStone',
+        draggable: false
+    });
+    marker = new google.maps.Marker({
+        position: { lat: -2.570501229717332, lng:  15.850967508911253 },
+        map: map,
+        title: 'BloodyGate',
+        draggable: false
+    });
+    marker = new google.maps.Marker({
+        position: { lat: -12.726084296948184, lng: 34.189453125 },
+        map: map,
+        title: 'the Whispers',
+        draggable: false
+    });
+    marker = new google.maps.Marker({
+        position: { lat: -20.632784250388013, lng:  30.146484375 },
+        map: map,
+        title: 'DragonStone',
+        draggable: false
+    });
+    marker = new google.maps.Marker({
+        position: { lat: -14.093957177836222, lng: 5.185546875 },
+        map: map,
+        title: 'Harrenhal',
+        draggable: false
+    });
+    marker = new google.maps.Marker({
+        position: { lat: -10.83330598364249, lng:  -8.876953125 },
+        map: map,
+        title: 'Riverrun',
+        draggable: false
+    });
+    marker = new google.maps.Marker({
+        position: { lat:  -29.07325157078329, lng:  10.983114043280239 },
+        map: map,
+        title: 'Kings Landing',
+        draggable: false
+    });
+    marker = new google.maps.Marker({
+        position: { lat:-24.044245063020245, lng:-29.62235470671976 },
+        map: map,
+        title: 'Casterly Rock',
+        draggable: false
+    });
+    marker = new google.maps.Marker({
+        position: { lat:-17.3063681176266, lng:-32.61063595671976 },
+        map: map,
+        title: 'Faircastle',
+        draggable: false
+    });
+    marker = new google.maps.Marker({
+        position: { lat:-24.044245063020245, lng: -29.62235470671976 },
+        map: map,
+        title: 'Casterly Rock',
+        draggable: false
+    });
+    marker = new google.maps.Marker({
+        position: { lat: 39.77663690896752, lng: 12.389364043280239 },
+        map: map,
+        title: 'Hornwood',
+        draggable: false
+    });
+    marker = new google.maps.Marker({
+        position: { lat: 46.19672399422663, lng:  7.66280154328024 },
+        map: map,
+        title: 'The Dreadfort',
+        draggable: false
+    });
+    marker = new google.maps.Marker({
+        position: { lat: -46.96923138779842, lng: -18.81848927557678 },
+        map: map,
+        title: 'HighGarden',
+        draggable: false
+    });
+    marker = new google.maps.Marker({
+        position: { lat: -57.49874960095754, lng:  -29.642100556410924 },
+        map: map,
+        title: 'Ghost hill',
+        draggable: false
+    });
+    marker = new google.maps.Marker({
+        position: { lat:  -48.7051714243519, lng:  1.654074633577693 },
+        map: map,
+        title: 'Blackhaven',
+        draggable: false
+    });
+    marker = new google.maps.Marker({
+        position: { lat: -39.520651593083336, lng:   1.654074633577693 },
+        map: map,
+        title: 'Grassy vale',
+        draggable: false
+    });
+    marker = new google.maps.Marker({
+        position: { lat:  -55.00915529806209, lng:  -30.238199714500922 },
+        map: map,
+        title: 'Oldtown',
+        draggable: false
+>>>>>>> kevin
+    });
+
+
+
+}
+*/
+
+var westerosMap;
+function fillMap(map) {
+
+    westerosMap=map;
+
+    marker = new google.maps.Marker({
+        position: { lat: 60.90262003187983, lng:10.283203125 },
+        map: westerosMap,
+        title: 'Castle black',
+        draggable: false
+    });
+    marker = new google.maps.Marker({
+        position: { lat: 43.751298804563255, lng: -2.9443359375 },
+        map: westerosMap,
+        title: 'Winterfell',
+        draggable: false
+    });
+    getRoute();
+}
+function getRoute() {
+    $.ajax({
+        url: 'http://localhost:3377/testRou',
+        data: {
+            format: 'json'
+        },
+        error: function (e) {
+            console.error(e);
+
+        },
+        success: function (data) {
+            fillRoute(data);
+        },
+        type: 'GET'
+    });
+}
+function fillRoute(data) {
+    $.each(data, function (index, value) {
+        var flight = new google.maps.Polyline({
+            path: [ {lat: value.locationOne.latitude, lng:value.locationOne.longitude},{lat: value.locationTwo.latitude, lng:value.locationTwo.longitude}],
+            geodesic: true,
+            strokeColor: '#FF0000',
+            strokeOpacity: 1.0,
+            strokeWeight: 2
+        });
+
+        flight.setMap(westerosMap)
     });
 }
