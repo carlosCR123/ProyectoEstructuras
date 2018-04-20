7af43d893a878bda0eda359f6389781654fd45b2
Jorge <jorgeda@gmail.com>
2018-04-19 23:38:41 -0600

    Modificaciones en los servicios del API y el controlador de esos servicios

--- src/main/java/com/cenfotec/westerosmap/web/MapController.java
+++ src/main/java/com/cenfotec/westerosmap/web/MapController.java
@@ -52,4 +52,25 @@ public class MapController {
         return null;
     }
 
+    @RequestMapping(method = RequestMethod.POST, value = "/locationByName")
+    @ResponseBody
+    Location getLocationByName(HttpServletResponse res, @RequestBody Location location){
+        Location fullLocation = serviceManager.getLocationByName(location);
+        if (fullLocation != null) return fullLocation;
+        res.setStatus(HttpServletResponse.SC_NO_CONTENT);
+        return null;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/shortestPath")
+    @ResponseBody
+    List<Route> getShortestPath(HttpServletResponse res, @RequestBody Location[] location){
+        if(location.length != 2){
+            res.setStatus(HttpServletResponse.SC_PRECONDITION_FAILED);
+            return null;
+        }
+        List<Route> routes = serviceManager.getShortestPath(location[0], location[1]);
+        if(routes != null) return routes;
+        res.setStatus(HttpServletResponse.SC_NO_CONTENT);
+        return null;
+    }
 }
--- src/main/java/com/cenfotec/westerosmap/web/MapServiceManager.java
+++ src/main/java/com/cenfotec/westerosmap/web/MapServiceManager.java
@@ -1,5 +1,8 @@
 package com.cenfotec.westerosmap.web;
 
+import com.cenfotec.westerosmap.datastructures.Graph;
+import com.cenfotec.westerosmap.datastructures.GraphArc;
+import com.cenfotec.westerosmap.datastructures.GraphVertex;
 import com.cenfotec.westerosmap.entities.Location;
 import com.cenfotec.westerosmap.entities.Route;
 import com.cenfotec.westerosmap.repositories.ILocationRepository;
@@ -36,27 +39,82 @@ public class MapServiceManager {
     }
 
     public List<Route> getRoutesOfLocation(Location location){
-        List<Route> routes = routeRepository.findByLocationOrigin(location.getId());
-        if(!routes.isEmpty()){
-            return routes;
+        Location fullLocation = locationRepository.findByName(location.getName());
+        if(fullLocation != null){
+            List<Route> routes = routeRepository.findRoutesOfLocationByLocationID(fullLocation.getId());
+            if(!routes.isEmpty()){
+                return routes;
+            }
         }
         return null;
     }
 
     public List<Location> getNeighborLocations(Location location){
-        List<Route> routes = routeRepository.findByLocationOrigin(location.getId());
-        List<Location> locations = new ArrayList<>();
-        if(!routes.isEmpty()){
-            for (Route route : routes) {
-                if(route.getLocationOne().getId() == location.getId()){
-                    locations.add(route.getLocationTwo());
-                }else{
-                    locations.add(route.getLocationOne());
+        Location fullLocation = locationRepository.findByName(location.getName());
+        if(fullLocation != null){
+            List<Route> routes = routeRepository.findRoutesOfLocationByLocationID(fullLocation.getId());
+            List<Location> locations = new ArrayList<>();
+            if(!routes.isEmpty()){
+                for (Route route : routes) {
+                    if(route.getLocationOne().getId() == fullLocation.getId()){
+                        locations.add(route.getLocationTwo());
+                    }else{
+                        locations.add(route.getLocationOne());
+                    }
                 }
+                return locations;
             }
-            return locations;
         }
         return null;
+    }
+
+    public Location getLocationByName(Location location){
+        return locationRepository.findByName(location.getName());
+    }
+
+    public List<Route> getShortestPath(Location origin, Location destiny){
+        origin = locationRepository.findByName(origin.getName());
+        destiny = locationRepository.findByName(destiny.getName());
+        if(origin != null && destiny != null){
+            List<Location> allLocations = locationRepository.findAll();
+            origin = replaceLocationWithObjectInList(origin, allLocations);
+            destiny = replaceLocationWithObjectInList(destiny, allLocations);
+            Graph graph = initilizeGraph(allLocations);
+            GraphVertex originVertex = findVertexFromLocation(graph.getVertexes(), origin);
+            GraphVertex destinyVertex = findVertexFromLocation(graph.getVertexes(), destiny);
+            return graph.findShortestPath(originVertex, destinyVertex);
+        }
+        return null;
+    }
+
+    private GraphVertex findVertexFromLocation(ArrayList<GraphVertex> vertexes, Location location) {
+        for (GraphVertex vertex : vertexes) {
+            if(location.getId() == vertex.getData().getId()) return vertex;
+        }
+        return null;
+    }
+
+    private Graph initilizeGraph(List<Location> allLocations) {
+        Graph graph = new Graph();
+        for (Location location : allLocations) {
+            ArrayList<GraphArc> arcs = new ArrayList<>();
+            List<Route> routesOfLocation = routeRepository.findRoutesOfLocationByLocationID(location.getId());
+            for (Route route : routesOfLocation) {
+                arcs.add(new GraphArc(route));
+            }
+            GraphVertex vertex = new GraphVertex(location, arcs);
+            graph.insertVertex(vertex);
+        }
+        return graph;
+    }
+
+    private Location replaceLocationWithObjectInList(Location object, List<Location> list){
+        for (Location item : list) {
+            if(object.getId() == item.getId()){
+                return item;
+            }
+        }
+        return object;
     }
 
 
